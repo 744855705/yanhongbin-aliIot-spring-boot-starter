e6121967fe01620d203305af759bb999abb7c93f
yanhongbin <744855705@qq.com>
2020-04-09 01:00:23 +0800

    修改默认消息处理器

---
+++ src/main/java/com/yanhongbin/aliiot/message/service/DefaultMessageProcessorConfig.java
@@ -0,0 +1,11 @@
+package com.yanhongbin.aliiot.message.service;
+
+/**
+ * Created with IDEA
+ * description :
+ *
+ * @author ：yanhongbin
+ * @date : Created in 2020/4/9 12:51 上午
+ */
+public class DefaultMessageProcessorConfig {
+}
